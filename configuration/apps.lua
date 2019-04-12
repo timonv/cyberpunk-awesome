697bcee15632d12d575ddd31acd22ac80f259c8e
Timon Vonk <timonv@gmail.com>
2019-04-12 14:07:32 +0200

    Re enable gnome keyring

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -13,6 +13,6 @@ return {
   run_on_start_up = {
     'compton --config ' .. filesystem.get_configuration_dir() .. '/configuration/compton.conf',
     'xfce4-power-manager', -- Power manager
-    -- '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)' -- credential manager
+    '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)' -- credential manager
   }
 }
